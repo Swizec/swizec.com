196ee650e0dbc368edb387b2457138dbdb481147
Swizec Teller <swizec@swizec.com>
2020-04-12 17:28:46 -0700

    All the content

---
+++ src/gatsby-remark-social-card.js
@@ -0,0 +1,33 @@
+// Custom plugin that generates social cards
+// take from frontmatter.hero by default
+// generate custom one otherwise
+
+const path = require("path")
+const slugify = require("slugify")
+const fsExtra = require("fs-extra")
+
+module.exports = async ({ markdownNode, markdownAST, getNode }) => {
+  const frontmatter = markdownNode.frontmatter
+
+  if (frontmatter && frontmatter.hero && frontmatter.title) {
+    const heroPath = path.posix.join(
+      getNode(markdownNode.parent).dir,
+      frontmatter.hero
+    )
+    const ext = heroPath.split(".").pop()
+    const filename = `${slugify(frontmatter.title)}.${ext}`
+
+    const newPath = path.join(process.cwd(), "public", "social-cards", filename)
+
+    try {
+      await fsExtra.ensureDir(
+        path.join(process.cwd(), "public", "social-cards")
+      )
+      await fsExtra.copy(heroPath, newPath)
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  return markdownAST
+}
