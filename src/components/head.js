97fea66e509e76619b2fe1dfa25efcf2f1497f94
escapemanuele <escapemanuele@gmail.com>
2020-07-20 10:29:15 +0200

    add various og

--- src/components/head.js
+++ src/components/head.js
@@ -48,6 +48,8 @@ export default (props) => {
       <meta name="twitter:description" content={description} />{" "}
       <meta name="twitter:image" content={image} />
       <meta property="og:title" content={title} />{" "}
+      <meta property="og:description" content={description} />{" "}
+      <meta property="og:type" content="article" />{" "}
       <meta property="og:url" content={url} />{" "}
       <meta property="og:image" content={image} />
     </Helmet>
