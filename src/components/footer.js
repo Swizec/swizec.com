4bc7ebbff803faa83bd50531191dae8cef0da1c8
Swizec Teller <swizec@swizec.com>
2020-08-15 17:40:39 -0700

    Finish homepage and add footer nav

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { Box, Link } from "rebass"
+import { Box, Link, Flex } from "rebass"
 
 export default (props) => (
-  <Box as="footer" py={5} color="background" bg="text">
+  <Box as="footer" py={5} color="background" bg="text" flexDirection="row">
     <Box
       sx={{
         maxWidth: "wide",
@@ -10,18 +10,40 @@ export default (props) => (
         px: 3,
       }}
     >
-      Created by
-      <Link
-        href="https://swizec.com"
-        variant="nav"
-        style={{ textDecoration: "underline" }}
-      >
-        Swizec
-      </Link>
-      with ❤️
-      <Link href="/" variant="nav">
-        swizec.com
-      </Link>
+      <Flex flexDirection="row" justifyContent="space-between">
+        <Box>
+          Created by
+          <Link
+            href="https://swizec.com"
+            variant="nav"
+            style={{ textDecoration: "underline" }}
+          >
+            Swizec
+          </Link>
+          with ❤️
+          <Link href="/" variant="nav">
+            swizec.com
+          </Link>
+        </Box>
+
+        <Box>
+          <Link href="/blog" variant="nav">
+            Blog
+          </Link>
+          <Link href="/books" variant="nav">
+            Books
+          </Link>
+          <Link href="/courses" variant="nav">
+            Courses
+          </Link>
+          <Link href="/workshops" variant="nav">
+            Workshops
+          </Link>
+          <Link href="/about" variant="nav">
+            About
+          </Link>
+        </Box>
+      </Flex>
     </Box>
   </Box>
 )
