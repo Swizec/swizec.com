673e1aa974b383bfbb9b92746711557d1f855b8f
Swizec Teller <swizec@swizec.com>
2020-08-17 07:46:12 -0700

    Fix bug in observer code

--- src/YoutubeEmbedderTransformer.js
+++ src/YoutubeEmbedderTransformer.js
@@ -8,25 +8,23 @@ const getHTML = (url) => {
 
   const newUrl = `https://www.youtube-nocookie.com/embed/${videoId}?rel=0`
 
-  return `
-    <div>
-    <div className="youtube-embed" id="${domId}">
-        
-    </div>
-    <script>
-        const observer = new IntersectionObserver(
-            ([entry]) => {
-                document.getElementById("${domId}").innerHTML = '<iframe title="Youtube embed" width="100%" height="100%" src="${newUrl}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen="">
-        </iframe>';
-            },
-            {
-                root: null,
-                rootMargin: '0px',
-                threshold: 1
-            }
-        )
-        observer.observe(document.getElementById("${domId}"))
-    </script>
+  return `<div>
+        <div className="youtube-embed" id="${domId}">
+        </div>
+        <script>
+            const observer = new IntersectionObserver(
+                ([entry]) => {
+                    console.log("IN VIEW", entry);
+                    document.getElementById("${domId}").innerHTML = '<iframe title="Youtube embed" width="100%" height="100%" src="${newUrl}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>';
+                },
+                {
+                    root: null,
+                    rootMargin: '0px',
+                    threshold: 1
+                }
+            )
+            observer.observe(document.getElementById("${domId}"))
+        </script>
     </div>`
 }
 
