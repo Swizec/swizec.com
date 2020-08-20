3c21081e57ce5b1593af9de586455f9407e63946
Swizec Teller <swizec@swizec.com>
2020-08-20 05:21:03 -0700

    Proof-of-concept thumbnail first embeds

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,1 +1,33 @@
 export { wrapPageElement } from "./src"
+
+function addJS(jsCode) {
+  var s = document.createElement(`script`)
+
+  s.type = `text/javascript`
+  s.innerText = jsCode
+  document.getElementsByTagName(`head`)[0].appendChild(s)
+}
+
+export const onRouteUpdate = () => {
+  if (document.querySelector("[data-embed-url]")) {
+    addJS(`
+for (let embed of document.querySelectorAll('[data-embed-url]')) {
+    embed.addEventListener('click', (event) => {
+        const embed = event.currentTarget;
+
+        const type = embed.dataset.embedType;
+        const url = embed.dataset.embedUrl;
+        const dimensions = embed.getBoundingClientRect();
+
+        if (type === 'youtube') {
+            const el = document.createElement('p');
+
+            el.innerHTML = '<iframe title="Youtube embed" width="'+dimensions.width+'px" height="'+dimensions.height+'px" src="'+url+'" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>';
+
+            embed.replaceWith(el);
+        }
+    })
+}
+        `)
+  }
+}
--- src/YoutubeEmbedderTransformer.js
+++ src/YoutubeEmbedderTransformer.js
@@ -1,16 +1,28 @@
+const fetch = require("isomorphic-fetch")
 const { v4: uuidv4 } = require("uuid")
 
 const regex = /^https:\/\/www\.youtube\.com\/watch\?v=(\S+)$/
 
-const getHTML = (url) => {
+async function getThumbnail(targetUrl) {
+  const url = `https://pifc233qp6.execute-api.us-east-1.amazonaws.com/dev/embed?url=${targetUrl}`
+
+  const res = await fetch(url).then((res) => res.json())
+
+  return res.url
+}
+
+const getHTML = async (url) => {
   const videoId = url.trim().match(regex)[1]
   const domId = uuidv4()
 
   const newUrl = `https://www.youtube-nocookie.com/embed/${videoId}?rel=0`
+  const imageUrl = await getThumbnail(url)
 
-  return `<div className="youtube-embed" id="${domId}">
-            <iframe title="Youtube embed" width="100%" height="100%" src="${newUrl}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
-        </div>`
+  return `<img src="${imageUrl}" id="${domId}" data-embed-url="${newUrl}" data-embed-type="youtube" />`
+
+  //   return `<div className="youtube-embed" id="${domId}">
+  //             <iframe title="Youtube embed" width="100%" height="100%" src="${newUrl}" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe>
+  //         </div>`
 }
 
 const name = "YouTube"
