94d57b65b11b12fe218de29a334ecd73e08a6e3c
Swizec Teller <swizec@swizec.com>
2020-08-24 07:27:16 -0700

    Fix the image fixer script

--- src/scripts/download-images.js
+++ src/scripts/download-images.js
@@ -12,75 +12,100 @@ const glob = require("glob")
 const fetch = require("node-fetch")
 const util = require("util")
 const streamPipeline = util.promisify(require("stream").pipeline)
+const prettier = require("prettier")
 
 const articlesPath = "./src/pages/blog"
 
 const ACCEPTED_FILES = ["jpg", "jpeg", "png", "gif", "svg"]
 
-glob(articlesPath + "/*/*.mdx", {}, (err, files) => {
-  files.forEach(async (file) => {
-    const parentDirectory = path.dirname(file)
+glob(articlesPath + "/*/*.mdx", {}, async (err, files) => {
+  await Promise.all(
+    files.map(async (file) => {
+      console.log(file)
 
-    const mdxFile = await read(file)
+      const parentDirectory = path.dirname(file)
 
-    //I process the file to get the AST
-    const contents = await remark()
-      .use(mdx)
-      .use(frontmatter)
-      .use(() => async (tree) => {
-        let nodes = []
+      const mdxFile = await read(file)
 
-        visit(tree, ["image"], async (node) => {
-          if (node.url && node.url.startsWith("http")) {
-            nodes.push(node)
-          }
-        })
+      //I process the file to get the AST
+      const contents = await new Promise((resolve, reject) => {
+        remark()
+          .use(mdx)
+          .use(frontmatter)
+          .use(() => async (tree) => {
+            let nodes = []
 
-        await Promise.all(
-          nodes.map(async function (node) {
-            let title =
-              node.title || node.alt || node.url.slice(node.url.length - 10)
-            title += Math.random().toString(20).substr(2, 6);
-            const slugTitle = slugify(title, { remove: /[*+~.()'"!?/:@,]/g })
+            visit(tree, ["image"], async (node) => {
+              if (node.url && node.url.startsWith("http")) {
+                nodes.push(node)
+              }
+            })
 
-            const imagesPath = path.join(parentDirectory, "img")
-            const destFileWithoutExtension = path.join(imagesPath, slugTitle)
-
-            try {
-              await fsExtra.ensureDir(imagesPath)
-              await downloadFile(node.url, destFileWithoutExtension)
-                .then((fileInfo) => {
-                  console.log(
-                    `Image ${slugTitle}.${fileInfo.extension} downloaded in ${imagesPath}`
-                  )
-                  node.url = `./img/${slugTitle}.${fileInfo.extension}`
+            await Promise.all(
+              nodes.map(async function (node) {
+                let title =
+                  node.title || node.alt || node.url.slice(node.url.length - 10)
+                title += Math.random().toString(20).substr(2, 6)
+                const slugTitle = slugify(title, {
+                  remove: /[*+~.()'"!?/:@,]/g,
                 })
-                .catch((err) =>
+
+                const imagesPath = path.join(parentDirectory, "img")
+                const destFileWithoutExtension = path.join(
+                  imagesPath,
+                  slugTitle
+                )
+
+                try {
+                  await fsExtra.ensureDir(imagesPath)
+                  await downloadFile(node.url, destFileWithoutExtension)
+                    .then((fileInfo) => {
+                      console.log(
+                        `Image ${slugTitle}.${fileInfo.extension} downloaded in ${imagesPath}`
+                      )
+                      node.url = `./img/${slugTitle}.${fileInfo.extension}`
+                    })
+                    .catch((err) =>
+                      console.log(
+                        `${chalk.red(
+                          "Error downloading image"
+                        )} in ${imagesPath} from ${node.url}: `,
+                        err
+                      )
+                    )
+                } catch (e) {
                   console.log(
                     `${chalk.red(
                       "Error downloading image"
                     )} in ${imagesPath} from ${node.url}: `,
-                    err
+                    e
                   )
-                )
-            } catch (e) {
-              console.log(
-                `${chalk.red(
-                  "Error downloading image"
-                )} in ${imagesPath} from ${node.url}: `,
-                e
-              )
+                }
+              })
+            )
+          })
+          .process(mdxFile, (err, markdown) => {
+            if (err) {
+              reject(err)
+            } else {
+              let content = markdown.contents
+              content = content
+                .replace(/(?<=https?:\/\/.*)\\_(?=.*\n)/g, "_")
+                .replace(/^\<(http.*)\>$/gm, "$1")
+              content = prettier.format(content, { parser: "mdx" })
+
+              resolve(content)
             }
           })
-        )
       })
-      .process(mdxFile)
 
-    await write({
-      path: file,
-      contents,
+      await write({
+        path: file,
+        contents,
+      })
     })
-  })
+  )
+  console.log("Done.")
 })
 
 function downloadFile(url, filePath) {
