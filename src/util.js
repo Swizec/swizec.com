8767c57cb59bf79c34b06aa24f1010f586d15da1
Emanuele <escapemanuele@gmail.com>
2021-02-17 22:42:46 +0100

    removed unused files

--- src/util.js
+++
@@ -1,9 +0,0 @@
-export function currentLocation({ location }) {
-  if (typeof window !== "undefined") {
-    return window.location.pathname
-  } else if (location) {
-    return location.pathname
-  } else {
-    return null
-  }
-}
