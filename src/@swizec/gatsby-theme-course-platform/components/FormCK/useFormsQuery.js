790d2ce7fe57d179098e9c0dba273e2a16e41430
Swizec Teller <swizec@swizec.com>
2021-04-06 06:04:18 -0700

    forms query

---
+++ src/@swizec/gatsby-theme-course-platform/components/FormCK/useFormsQuery.js
@@ -0,0 +1,20 @@
+import { useStaticQuery, graphql } from "gatsby"
+
+export const useFormsQuery = () => {
+  // change this query when you add forms
+  const { site } = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          convertkit {
+            defaultFormId
+            serverlessHandbookFormId
+            seniorMindsetFormId
+          }
+        }
+      }
+    }
+  `)
+
+  return site.siteMetadata.convertkit
+}
