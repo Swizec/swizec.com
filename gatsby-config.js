4ea44a7411127391db9c432b9cd6f5af4cc171b0
Swizec Teller <swizec@swizec.com>
2021-05-06 20:07:23 -0700

    Add gatsby cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -33,66 +33,10 @@ module.exports = {
       id: "aab01040-bb89-40d9-8a2e-92ede0f8d82b",
     },
     siteUrl: "https://swizec.com",
-    // articles: {
-    //   title: `React for Dataviz`,
-    //   description: `
-    //     A monthly data visualization built with React, D3, and others.
-    //     Livecoded last Sunday of the month.
-    //     <a href="https://www.youtube.com/channel/UCoyHgaeLLI7Knp7LDHOwZMw">
-    //       Join live
-    //     </a>
-    //     or subscribe to the newsletter 💌
-    //   `,
-    //   titleSeo: `React for Data Visualization Articles`,
-    //   descriptionSeo: `A monthly data visualization built with React, D3, and others`,
-    // },
   },
   plugins: [
     "@swizec/gatsby-theme-course-platform",
-    "gatsby-plugin-netlify",
-    // {
-    //   resolve: "gatsby-source-filesystem",
-    //   options: {
-    //     name: "posts",
-    //     path: `${__dirname}/src/pages/blog`,
-    //   },
-    // },
-    // {
-    //   resolve: "gatsby-plugin-mdx",
-    //   options: {
-    //     extensions: [".mdx", ".md"],
-    //     remarkPlugins,
-    //     gatsbyRemarkPlugins: [
-
-    //       {
-    //         resolve: "gatsby-remark-embedder",
-    //         options: {
-    //           customTransformers: [YoutubeTransformer],
-    //           services: {
-    //             Instagram: {
-    //               accessToken: process.env.INSTAGRAM_OEMBED_TOKEN,
-    //             },
-    //           },
-    //         },
-    //       },
-    //     ],
-    //   },
-    // },
-    // add a gatsby-source-filesystem entry for every article's images
-    // have to filter out empty folders for Zeit
-    // ...fs
-    //   .readdirSync(`${__dirname}/src/pages/blog`)
-    //   .map((path) => `${__dirname}/src/pages/blog/${path}`)
-    //   .filter((path) => fs.lstatSync(path).isDirectory())
-    //   .map((path) => `${path}/img`)
-    //   .filter((path) => fs.existsSync(path) && fs.readdirSync(path).length > 0)
-    //   .map((path) => ({
-    //     resolve: "gatsby-source-filesystem",
-    //     options: {
-    //       path,
-    //     },
-    //   })),
-    // "gatsby-plugin-instagram-embed",
+    "gatsby-plugin-gatsby-cloud",
     {
       resolve: "gatsby-plugin-manifest",
       options: {
