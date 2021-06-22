949ea991b22d3fd444f3d620e8c43b895ef2259c
Swizec Teller <swizec@swizec.com>
2021-06-22 08:33:21 -0700

    Copy over redirects fixes

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,59 +1,72 @@
 const chalk = require("chalk")
+const fs = require("fs")
 
 const sharp = require("sharp")
 sharp.cache(false)
 sharp.simd(false)
 
-exports.createPages = ({ graphql, actions }) => {
+exports.onPreBootstrap = async ({ actions }) => {
+  // read _redirects
+  // call createRedirect for each
+
+  const redirects = fs.readFileSync("./static/_redirects").toString()
+
+  for (const line of redirects.split("\n")) {
+    if (line.trim().length > 0) {
+      // found a redirect
+      const [fromPath, toPath] = line.trim().split(/\s+/)
+      actions.createRedirect({
+        fromPath,
+        toPath,
+      })
+    }
+  }
+
+  console.log(`${chalk.green("success")} create redirects from _redirects`)
+}
+
+exports.createPages = async ({ graphql, actions }) => {
   const { createRedirect } = actions
 
-  return new Promise((resolve, reject) => {
-    resolve(
-      graphql(
-        `
-          {
-            allMdx(filter: { frontmatter: { redirect_from: { ne: null } } }) {
-              edges {
-                node {
-                  fields {
-                    slug
-                  }
-                  frontmatter {
-                    redirect_from
-                  }
-                }
-              }
+  const result = await graphql(`
+    {
+      allMdx(filter: { frontmatter: { redirect_from: { ne: null } } }) {
+        edges {
+          node {
+            fields {
+              slug
+            }
+            frontmatter {
+              redirect_from
             }
           }
-        `
-      ).then((result) => {
-        if (result.errors) {
-          console.log(result.errors) // eslint-disable-line no-console
-          reject(result.errors)
         }
+      }
+    }
+  `)
 
-        const allPosts = result.data.allMdx.edges
+  if (result.errors) {
+    console.log(result.errors) // eslint-disable-line no-console
+    throw result.errors
+  }
 
-        // For all posts with redirect_from frontmatter,
-        // extract all values and push to redirects array
-        allPosts.forEach((post) => {
-          const from = post.node.frontmatter.redirect_from
-          const to = `/blog${post.node.fields.slug}`
+  const allPosts = result.data.allMdx.edges
 
-          from.forEach((from) => {
-            createRedirect({
-              fromPath: from,
-              toPath: to,
-              isPermanent: true,
-              redirectInBrowser: true,
-            })
-          })
-        })
+  // For all posts with redirect_from frontmatter,
+  // extract all values and push to redirects array
+  allPosts.forEach((post) => {
+    const from = post.node.frontmatter.redirect_from
+    const to = `/blog${post.node.fields.slug}`
 
-        resolve(
-          console.log(`${chalk.green("success")} create redirects`) // eslint-disable-line no-console
-        )
+    from.forEach((from) => {
+      createRedirect({
+        fromPath: from,
+        toPath: to,
+        isPermanent: true,
+        redirectInBrowser: true,
       })
-    )
+    })
   })
+
+  console.log(`${chalk.green("success")} create redirects`) // eslint-disable-line no-console
 }
