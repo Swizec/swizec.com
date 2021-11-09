8991ccb43fb861effb421809864a4f626313d71c
Swizec Teller <swizec@swizec.com>
2021-11-09 10:02:47 -0800

    Load fonts

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -1,5 +1,7 @@
 import merge from "deepmerge"
 import { courseTheme } from "@swizec/gatsby-theme-course-platform"
+import "typeface-exo"
+import "typeface-yrsa"
 
 const customTheme = {
   header: {
