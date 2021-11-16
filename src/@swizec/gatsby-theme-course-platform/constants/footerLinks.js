f1fcf3648ce7df8b7c594dc70805a68e668bd8a9
Swizec Teller <swizec@swizec.com>
2021-11-16 10:14:40 -0800

    boop footer

--- src/@swizec/gatsby-theme-course-platform/constants/footerLinks.js
+++ src/@swizec/gatsby-theme-course-platform/constants/footerLinks.js
@@ -1,22 +1,22 @@
 export default [
-    {
-        Link: '/blog',
-        Title: 'Blog',
-    },
-    {
-        Link: '/books',
-        Title: 'Books',
-    },
-    {
-        Link: '/courses',
-        Title: 'Courses',
-    },
-    {
-        Link: '/workshops',
-        Title: 'Workshops',
-    },
-    {
-        Link: '/about',
-        Title: 'About',
-    },
-] 
+  {
+    Link: "/blog",
+    Title: "Articles",
+  },
+  {
+    Link: "/books",
+    Title: "Books",
+  },
+  {
+    Link: "/courses",
+    Title: "Courses",
+  },
+  {
+    Link: "/workshops",
+    Title: "Workshops",
+  },
+  {
+    Link: "/about",
+    Title: "About",
+  },
+]
