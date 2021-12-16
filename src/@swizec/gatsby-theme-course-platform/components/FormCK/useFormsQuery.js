f55946f6adfef1d0638a0778744f153181fc4704
Chris Brooks <chris.brooks@vervini.com>
2021-12-16 18:54:09 +0200

    Added ServerlessFormId

--- src/@swizec/gatsby-theme-course-platform/components/FormCK/useFormsQuery.js
+++ src/@swizec/gatsby-theme-course-platform/components/FormCK/useFormsQuery.js
@@ -17,6 +17,7 @@ export const useFormsQuery = () => {
             ReactCUFormId
             BackendWebFormId
             IndieHackingFormId
+            ServerlessFormId
           }
         }
       }
