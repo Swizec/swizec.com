027ca8f74c81d286cfbff2517042638ba0c7a814
Swizec Teller <swizec@swizec.com>
2023-12-29 13:09:17 -0800

    Create embeddings table

--- bin/index-articles.ts
+++ bin/index-articles.ts
@@ -1,1 +1,9 @@
-console.log(process.env.POSTGRES_URL)
+import { sql } from "@vercel/postgres"
+
+// await sql`CREATE EXTENSION vector;`
+// await sql`CREATE TABLE IF NOT EXISTS article_embeddings (
+//     url TEXT PRIMARY KEY,
+//     title TEXT,
+//     published_date DATE,
+//     embedding VECTOR(1536)
+// )`
