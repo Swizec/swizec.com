160b09cd37b344c617db7a6bed4a5d813f75fac9
Swizec Teller <swizec@swizec.com>
2023-12-29 13:22:05 -0800

    Parse articles

--- bin/index-articles.ts
+++ bin/index-articles.ts
@@ -1,4 +1,7 @@
 import { sql } from "@vercel/postgres"
+import fs from "fs"
+import path from "path"
+import matter from "gray-matter"
 
 // await sql`CREATE EXTENSION vector;`
 // await sql`CREATE TABLE IF NOT EXISTS article_embeddings (
@@ -7,3 +10,36 @@ import { sql } from "@vercel/postgres"
 //     published_date DATE,
 //     embedding VECTOR(1536)
 // )`
+
+function findIndexMDXFiles(dir: string, fileList: string[] = []): string[] {
+  const files = fs.readdirSync(dir)
+
+  files.forEach((file) => {
+    const filePath = path.join(dir, file)
+    const fileStat = fs.statSync(filePath)
+
+    if (fileStat.isDirectory()) {
+      findIndexMDXFiles(filePath, fileList)
+    } else if (path.basename(filePath) === "index.mdx") {
+      fileList.push(filePath)
+    }
+  })
+
+  return fileList
+}
+
+async function indexArticle(path: string) {
+  console.log(`Processing ${path}`)
+
+  const file = Bun.file(path)
+  const { data: frontmatter, content } = matter(await file.text())
+
+  console.log(frontmatter)
+  console.log(content)
+}
+
+const articles = findIndexMDXFiles(`${import.meta.dir}/../src/pages/blog`)
+
+for (const article of articles) {
+  await indexArticle(article)
+}
